51fcfdd80a455d79faea0ee8ff731284013af3e9
mohnad banat <75941340+mohnad-0b@users.noreply.github.com>
2022-09-27 04:15:54 +0300

    Update index.php

--- Projects/Project_03/app/index.php
+++ Projects/Project_03/app/index.php
@@ -9,7 +9,7 @@
         $user = $_POST['user'];
         $pass = $_POST['pass'];
 
-        $con = mysqli_connect("localhost","root","","app");
+        $con = mysqli_connect("localhost","root","root","app");
         $sql = "SELECT * FROM users WHERE username = '$user' AND `password` = '$pass'";
         $result = mysqli_query($con,$sql);
         $row = mysqli_num_rows($result);
